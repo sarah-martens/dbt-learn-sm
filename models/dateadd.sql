1cbfa7211cd7c39249c446ce9f16dae0742bb7d8
sarah.martens@breakthroughfuel.com <sarah.martens@breakthroughfuel.com>
2022-01-04 16:41:52 +0000

    deleted dateadd.sql and the limit_data_in_dev marco because it was failing because of the packages and i needed to dbt run

--- models/dateadd.sql
+++
@@ -1,3 +0,0 @@
-{% macro exasol__dateadd(datepart, interval, from_date_or_timestamp) %}
-    cast({{ from_date_or_timestamp }} as date) +  ((interval '1' {{ datepart }}) * ({{ interval }}))
-{% endmacro %}
